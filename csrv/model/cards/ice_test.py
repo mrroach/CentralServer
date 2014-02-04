98ce6092ac7b4a0ce670a3fb1723dbbd59f481a8
Mark Roach <mrroach@okmaybe.com>
2014-02-03 20:56:02 -0800

    add tests for server modifiers for ice

--- csrv/model/cards/ice_test.py
+++ csrv/model/cards/ice_test.py
@@ -8,8 +8,8 @@ class IceTest(test_base.TestBase):
   def setUp(self):
     test_base.TestBase.setUp(self)
     self.card = ice.Ice(self.game, self.corp)
-    server = self.game.corp.new_remote_server()
-    server.install(self.card)
+    self.server = self.game.corp.new_remote_server()
+    self.server.install(self.card)
     self.card.COST = 4
     self.card.STRENGTH = 4
 
@@ -28,6 +28,12 @@ class IceTest(test_base.TestBase):
     mod.remove()
     self.assertEqual(4, self.card.cost)
 
+  def test_cost_honors_global_modifiers(self):
+    mod = modifiers.IceRezCostModifier(self.game, -1, server=self.server)
+    self.assertEqual(3, self.card.cost)
+    mod.remove()
+    self.assertEqual(4, self.card.cost)
+
   def test_strength_works_without_modifiers(self):
     self.assertEqual(4, self.card.strength)
 
@@ -39,6 +45,12 @@ class IceTest(test_base.TestBase):
 
   def test_strength_honors_global_modifiers(self):
     mod = modifiers.IceStrengthModifier(self.game, -1)
+    self.assertEqual(3, self.card.strength)
+    mod.remove()
+    self.assertEqual(4, self.card.strength)
+
+  def test_strength_honors_server_modifiers(self):
+    mod = modifiers.IceStrengthModifier(self.game, -1, server=self.server)
     self.assertEqual(3, self.card.strength)
     mod.remove()
     self.assertEqual(4, self.card.strength)
