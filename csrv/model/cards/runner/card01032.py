a08ff389d027dca5c7315981cf763aec8225983f
Mark Roach <mrroach@okmaybe.com>
2014-02-13 22:13:26 -0800

    add tag prevention card

--- csrv/model/cards/runner/card01032.py
+++ csrv/model/cards/runner/card01032.py
@@ -1,5 +1,23 @@
+from csrv.model import actions
+from csrv.model import timing_phases
 from csrv.model.cards import card_info
 from csrv.model.cards import resource
+
+
+class PreventATag(actions.Action):
+  DESCRIPTION = 'Prevent a tag'
+
+  def is_usable(self):
+    return (actions.Action.is_usable(self) and
+            bool(self.game.current_phase().tags))
+
+  def resolve(self, response=None, ignore_clicks=False, ignore_all_costs=False):
+    actions.Action.resolve(
+        self, response=response,
+        ignore_clicks=ignore_clicks,
+        ignore_all_costs=ignore_all_costs)
+    self.game.current_phase().tags -= 1
+    self.card.trash()
 
 
 class Card01032(resource.Resource):
@@ -17,5 +35,13 @@ class Card01032(resource.Resource):
   COST = 1
   IMAGE_SRC = '01032.png'
 
+  WHEN_INSTALLED_PROVIDES_CHOICES_FOR = {
+      timing_phases.TakeTags: 'prevent_tag_actions',
+  }
+
   def build_actions(self):
     resource.Resource.build_actions(self)
+    self._prevent_a_tag = PreventATag(self.game, self.player, self)
+
+  def prevent_tag_actions(self):
+    return [self._prevent_a_tag]
---
+++ csrv/model/cards/runner/card01032_test.py
@@ -0,0 +1,31 @@
+import unittest
+from csrv.model import deck
+from csrv.model import game
+from csrv.model import premade_decks
+from csrv.model import test_base
+from csrv.model import timing_phases
+from csrv.model.cards import card_info
+from csrv.model.cards.runner import card01032
+
+
+class TestCard01032(test_base.TestBase):
+  RUNNER_DECK = 'Shaper Core'
+
+  def setUp(self):
+    test_base.TestBase.setUp(self)
+    self.card = card01032.Card01032(self.game, self.game.runner)
+    self.game.runner.grip.add(self.card)
+
+  def test_prevent_tag(self):
+    self.game.runner.rig.add(self.card)
+    self.game.insert_next_phase(
+        timing_phases.TakeTags(self.game, self.runner, 2))
+    self.assertIn(self.card._prevent_a_tag, self.game.current_phase().choices())
+    self.game.resolve_current_phase(self.card._prevent_a_tag, None)
+    self.assertEqual(self.runner.heap, self.card.location)
+    self.game.resolve_current_phase(None, None)
+    self.assertEqual(1, self.runner.tags)
+
+
+if __name__ == '__main__':
+  unittest.main()
