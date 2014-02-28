3d4f5d1b477a1546af494d0122c7d2e5d4b830ae
Mark Roach <mrroach@okmaybe.com>
2014-02-27 18:19:05 -0800

    revert the autogen of events, but keep them sorted

--- csrv/model/events.py
+++ csrv/model/events.py
@@ -7,7 +7,6 @@ to subscribe to.
 """
 
 import re
-import sys
 from csrv.model import game_object
 
 CALLBACK_RE = re.compile(r'\B((?<=[a-z])[A-Z]|[A-Z](?=[a-z]))')
@@ -33,61 +32,188 @@ class BaseEvent(game_object.PlayerObject):
   __metaclass__ = EventMeta
 
 
-EVENTS = {
-    'BeginCorpGameSetup': 'The corp draws a starting hand.',
-    'CorpGameSetup': 'The corp may mulligan.',
-    'BeginRunnerGameSetup': 'The runner draws a starting hand.',
-    'RunnerGameSetup': 'The runner may mulligan.',
-    'CorpTurnBegin': 'The beginning of the Corp\'s turn.',
-    'CorpTurnEnd': 'The Corp\'s turn ends.',
-    'RunnerTurnEnd': 'The Runner\'s turn ends.',
-    'CorpTurnDraw': 'The corp must draw a card.',
-    'CorpActionPhase': 'The corp takes actions.',
-    'CorpDiscardPhase': 'The corp must discard down to max hand size.',
-    'GainACredit': 'A click for a credit.',
-    'InstallAgendaAssetUpgrade': 'An agenda, asset, or upgrade was installed.',
-    'InstallIce': 'Ice was installed.',
-    'UninstallCard': 'Generic event for any card being uninstalled.',
-    'UninstallIce': 'Ice was uninstalled.',
-    'InstallProgram': 'A program was installed.',
-    'InstallHardware': 'A program was installed.',
-    'DrawFromRnD': 'The corp draws a card from R&D.',
-    'DrawFromStack': 'The runner draws a card from the stack.',
-    'PurgeVirusCounters': 'The corp purges virus counters.',
-    'RezAssetUpgrade': 'The corp rezzes an asset or upgrade.',
-    'RezIce': 'The corp rezzes a piece of ice.',
-    'IceStrengthChanged': 'The strength of a piece of ice changed.',
-    'BeginRunnerTurnBegin': 'The very beginning of the runner\'s turn.',
-    'RunnerTurnBegin': 'The beginning of the Runner\'s turn.',
-    'RunnerActionPhase': 'The runner takes actions.',
-    'RunnerDiscardPhase': 'The runner must discard down to max hand size.',
-    'BeginEncounterIce_3_1':
-        'Phase 3.1 of an ancounter with a piece of ice begins.',
-    'BeginEncounterIce_3_2':
-        'Phase 3.2 of an ancounter with a piece of ice begins.',
-    'EndEncounterIce_3_2':
-        'Phase 3.2 of an encounter with a piece of ice ends.',
-    'BeginApproachIce_2_1': 'The runner approaches a piece of ice.',
-    'BeginApproachServer_4_1': 'The runner approaches a server.',
-    'ApproachServer_4_4': 'A run is successful.',
-    'BeginApproachServer_4_5': 'Beginning of the approach server 4.5 phase.',
-    'EndApproachServer_4_5': 'An access phase completes.',
-    'RunBegins': 'A run begins.',
-    'RunEnds': 'A run ends (either successfully or failed).',
-    'UnsuccessfulRun': 'A run ends unsuccessfully.',
-    'SuccessfulRun': 'A run ends successfully.',
-    'EndTakeBrainDamage': 'A brain damage phase ends.',
-    'EndTakeNetDamage': 'A net damage phase ends.',
-    'EndTraceCorpBoost': 'The corp finishes boosting a trace.',
-    'EndTraceRunnerBoost': 'The runner finishes boosting a trace.',
-    'ScoreAgenda': 'The corp scores an agenda.',
-    'StealAgenda': 'The runner steals an agenda.',
-    'PlayOperation': 'The corp plays an operation.',
-}
+###
+### Keep in alphabetical order, please.
+###
 
-# Create the classes based on the dictionary above and set them as module
-# attributes.
-_THIS_MODULE = sys.modules[__name__]
-for class_name, doc in EVENTS.iteritems():
-  class_obj = type(class_name, (BaseEvent,), {'__doc__': doc})
-  setattr(_THIS_MODULE, class_name, class_obj)
+class ApproachServer_4_4(BaseEvent):
+  """A run is successful."""
+
+
+class BeginApproachIce_2_1(BaseEvent):
+  """The runner approaches a piece of ice."""
+
+
+class BeginApproachServer_4_1(BaseEvent):
+  """The runner approaches a server."""
+
+
+class BeginApproachServer_4_5(BaseEvent):
+  """Beginning of the approach server 4.5 phase."""
+
+
+class BeginCorpGameSetup(BaseEvent):
+  """The corp draws a starting hand."""
+
+
+class BeginEncounterIce_3_1(BaseEvent):
+  """Phase 3.1 of an ancounter with a piece of ice begins."""
+
+
+class BeginEncounterIce_3_2(BaseEvent):
+  """Phase 3.2 of an ancounter with a piece of ice begins."""
+
+
+class BeginRunnerGameSetup(BaseEvent):
+  """The runner draws a starting hand."""
+
+
+class BeginRunnerTurnBegin(BaseEvent):
+  """The very beginning of the runner's turn."""
+
+
+class CorpActionPhase(BaseEvent):
+  """The corp takes actions."""
+
+
+class CorpDiscardPhase(BaseEvent):
+  """The corp must discard down to max hand size."""
+
+
+class CorpGameSetup(BaseEvent):
+  """The corp may mulligan."""
+
+
+class CorpTurnBegin(BaseEvent):
+  """The beginning of the Corp's turn."""
+
+
+class CorpTurnDraw(BaseEvent):
+  """The corp must draw a card."""
+
+
+class CorpTurnEnd(BaseEvent):
+  """The Corp's turn ends."""
+
+
+class DrawFromRnD(BaseEvent):
+  """The corp draws a card from R&D."""
+
+
+class DrawFromStack(BaseEvent):
+  """The runner draws a card from the stack."""
+
+
+class EndApproachServer_4_5(BaseEvent):
+  """An access phase completes."""
+
+
+class EndEncounterIce_3_2(BaseEvent):
+  """Phase 3.2 of an encounter with a piece of ice ends."""
+
+
+class EndTakeBrainDamage(BaseEvent):
+  """A brain damage phase ends."""
+
+
+class EndTakeNetDamage(BaseEvent):
+  """A net damage phase ends."""
+
+
+class EndTraceCorpBoost(BaseEvent):
+  """The corp finishes boosting a trace."""
+
+
+class EndTraceRunnerBoost(BaseEvent):
+  """The runner finishes boosting a trace."""
+
+
+class GainACredit(BaseEvent):
+  """A click for a credit."""
+
+
+class IceStrengthChanged(BaseEvent):
+  """The strength of a piece of ice changed."""
+
+
+class InstallAgendaAssetUpgrade(BaseEvent):
+  """An agenda, asset, or upgrade was installed."""
+
+
+class InstallHardware(BaseEvent):
+  """A program was installed."""
+
+
+class InstallIce(BaseEvent):
+  """Ice was installed."""
+
+
+class InstallProgram(BaseEvent):
+  """A program was installed."""
+
+
+class PlayOperation(BaseEvent):
+  """The corp plays an operation."""
+
+
+class PurgeVirusCounters(BaseEvent):
+  """The corp purges virus counters."""
+
+class RezAssetUpgrade(BaseEvent):
+  """The corp rezzes an asset or upgrade."""
+
+
+class RezIce(BaseEvent):
+  """The corp rezzes a piece of ice."""
+
+
+class RunBegins(BaseEvent):
+  """A run begins."""
+
+
+class RunEnds(BaseEvent):
+  """A run ends (either successfully or failed)."""
+
+
+class RunnerActionPhase(BaseEvent):
+  """The runner takes actions."""
+
+
+class RunnerDiscardPhase(BaseEvent):
+  """The runner must discard down to max hand size."""
+
+
+class RunnerGameSetup(BaseEvent):
+  """The runner may mulligan."""
+
+
+class RunnerTurnBegin(BaseEvent):
+  """The beginning of the Runner's turn."""
+
+
+class RunnerTurnEnd(BaseEvent):
+  """The Runner's turn ends."""
+
+
+class ScoreAgenda(BaseEvent):
+  """The corp scores an agenda."""
+
+
+class StealAgenda(BaseEvent):
+  """The runner steals an agenda."""
+
+
+class SuccessfulRun(BaseEvent):
+  """A run ends successfully."""
+
+
+class UninstallCard(BaseEvent):
+  """Generic event for any card being uninstalled."""
+
+
+class UninstallIce(BaseEvent):
+  """Ice was uninstalled."""
+
+
+class UnsuccessfulRun(BaseEvent):
+  """A run ends unsuccessfully."""
